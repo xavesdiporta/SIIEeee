1e16744aad11ea7022bff61a254b24c377d233dd
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2026-05-10 00:36:06 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
                 'lemon-squeezy/*',
             ]
         );
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
