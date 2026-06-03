548dea0b90f3d69ad2fe2625abcbaf3797127089
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2026-06-03 21:27:38 +0100

    one minor change

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-8 px-4 sm:px-6 lg:px-8 h-[calc(100vh-8rem)]">
+    <div class="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 h-[calc(100vh-8rem)]">
 
         @php
             $categories = [
