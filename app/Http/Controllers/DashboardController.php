d9dc884b43f52ec52531f070395475889c485ee6
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2026-09-09 00:48:30 +0100

    carbon import change

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\GoogleCalendarService;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
