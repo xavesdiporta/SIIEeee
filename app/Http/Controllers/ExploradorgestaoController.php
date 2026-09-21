c1bb333aeeb4b8c27aecc717b57806c57463cd69
Xavier Margarido <86982363+xavesdiporta@users.noreply.github.com>
2026-09-21 16:10:49 +0100

    Update ExploradorgestaoController.php

--- app/Http/Controllers/ExploradorgestaoController.php
+++ app/Http/Controllers/ExploradorgestaoController.php
@@ -36,7 +36,7 @@ class ExploradorgestaoController extends Controller
             ->groupBy('user_id')
             ->map(fn ($notas) => $notas->pluck('reference')->all());
 
-        return view('pages.exploradores', [
+        return view('pages.dashboards.exploradores', [
             'categorias'   => $categorias,
             'totalRefsAll' => $totalRefsAll,
             'exploradores' => $exploradores,
